7c186fdadaf081df512b9fa15cf9ed70646bbd97
Sankar Sivan <129701713+SankarSivan@users.noreply.github.com>
2025-04-19 17:15:12 +0530

    Update Phonepe_streamlit_app.py

--- Phonepe_streamlit_app.py
+++ Phonepe_streamlit_app.py
@@ -1,16 +1,7 @@
 import streamlit as st
-
-<<<<<<< HEAD
-
-
-import streamlit as st
 import pandas as pd
 
 
-import streamlit as st
-
-st.title("My PhonePe Streamlit App")
 =======
 st.title("🎈 My new app-123")
 
->>>>>>> c7685626c6709f02f42382cbf91bd14a84b5e85f
